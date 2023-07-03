38073280e62f93cbdac7cda73b10ed5313e940e1
hun1001 <l01022899266@gmail.com>
2023-07-03 18:41:19 +0900

    이동만 간단하게 제작중

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInput playerInput = null;
+    private InputAction _moveAction = null;
+
+    private void Awake()
     {
-        
+        playerInput = new PlayerInput();
+        _moveAction = playerInput.Player.Move;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        playerInput.Enable();
+
+        _moveAction.performed += ctx => Move(ctx.ReadValue<Vector2>());
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = _moveAction.ReadValue<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    private void Move(Vector2 moveInput)
+    {
+        Debug.Log(moveInput);
     }
 }
