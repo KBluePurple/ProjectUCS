2ea99da4e4d772c4926a6b01d493fd0e4d1f818e
hun1001 <l01022899266@gmail.com>
2023-08-16 13:50:00 +0900

    안올렸던 meta push

--- Assets/02.Scripts/Character/Base/CharacterBase.cs
+++ Assets/02.Scripts/Character/Base/CharacterBase.cs
@@ -24,17 +24,25 @@ public class CharacterBase : MonoBehaviour
 
     private Rigidbody2D _rigidbody2D = null;
     private Animator _animator = null;
+    private SpriteRenderer _spriteRenderer = null;
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _animator = transform.GetChild(0).GetComponent<Animator>();
+        _spriteRenderer = _animator.GetComponent<SpriteRenderer>();
     }
 
     public void Move(Vector2 direction)
     {
         _animator.SetBool("IsMove", direction.x != 0);
-        _rigidbody2D.velocity = new Vector2(direction.x * _moveSpeed, _rigidbody2D.velocity.y);
+
+        if (!_animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            _rigidbody2D.velocity = new Vector2(direction.x * _moveSpeed, _rigidbody2D.velocity.y);
+        }
+
+        _spriteRenderer.flipX = direction.x < 0;
     }
 
     public void Jump()
