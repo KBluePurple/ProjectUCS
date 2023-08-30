63a555736a5388f9a8fc0efb8e9cf2e968c78b2a
KBluePurple <42506968+BlWon@users.noreply.github.com>
2023-08-30 11:21:18 +0900

    캐릭터 움직임 입력도 동기화

--- Assets/02.Scripts/Network/NetworkPlayer.cs
+++ Assets/02.Scripts/Network/NetworkPlayer.cs
@@ -1,10 +1,20 @@
+using System;
 using ProjectUCS.Common.Data;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NetworkPlayer : MonoBehaviour
 {
+    [SerializeField] private CharacterBase characterBase;
+    private Keyboard _keyboard = null;
+
     public int UserId { get; set; }
     public bool IsLocalPlayer { get; set; }
+
+    private void Awake()
+    {
+        _keyboard = Keyboard.current;
+    }
 
     private void Start()
     {
@@ -21,14 +31,24 @@ public class NetworkPlayer : MonoBehaviour
 
     private void Update()
     {
-        if (IsLocalPlayer && NetworkRoom.IsInRoom)
-            NetworkManager.Instance.Send(new C2S.Room.MovePacket
+        if (!IsLocalPlayer || !NetworkRoom.IsInRoom) return;
+
+        NetworkManager.Instance.Send(new C2S.Room.MovePacket
+        {
+            Position = new Position
             {
-                Position = new Position
-                {
-                    X = transform.position.x,
-                    Y = transform.position.y
-                }
-            });
+                X = transform.position.x,
+                Y = transform.position.y
+            },
+            Horizontal = _keyboard.leftArrowKey.isPressed ? -1 : _keyboard.rightArrowKey.isPressed ? 1 : 0
+        });
+    }
+
+    public void Move(Position packetPosition, int packetHorizontal)
+    {
+        transform.position = new Vector3(packetPosition.X, packetPosition.Y, 0);
+        characterBase.Move(new Vector2(packetHorizontal, 0));
+
+        Debug.Log($"Move: {packetPosition.X}, {packetPosition.Y}, {packetHorizontal}");
     }
 }
